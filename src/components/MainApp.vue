8946fa360804659c2d0b3aec183bb845c13fe267
francescofalcone24 <cicciofalcon97@gmail.com>
2024-09-27 10:51:36 +0300

    creating pages and components

---
+++ src/components/MainApp.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+
+    name:'',
+
+    components:{
+
+    },
+
+    data() {
+        return {
+
+        };
+    },
+    methods:{
+
+    },
+
+    mounted(){
+
+    },
+};
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
