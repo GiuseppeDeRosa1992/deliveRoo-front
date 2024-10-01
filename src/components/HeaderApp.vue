0f595a10fade4432dae3a612a9831a038c55fc05
francescofalcone24 <cicciofalcon97@gmail.com>
2024-10-01 12:39:57 +0300

    jumboo and img

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -24,21 +24,75 @@ export default {
 </script>
 
 <template>
-    <section>
+    <!-- *******HEADER****** -->
 
-        <div class="container p-0 h-100 d-flex justify-content-between">
-            <div class="left-side h-100">
-                <img src="../assets/public/img/Delive8.png" alt="">
-            </div>
-            <div class="right-side h-100 text-end align-content-center">
-                <div class="carrello align-content-center">
-                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+    <header>
+
+        <section>
+
+            <div class="container p-0 h-100 d-flex justify-content-between">
+                <div class="left-side h-100">
+                    <img src="../assets/public/img/Delive8.png" alt="">
                 </div>
+                <div class="right-side h-100 text-end align-content-center">
+                    <div class="carrello align-content-center">
+                        <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                    </div>
+                </div>
+
             </div>
 
-        </div>
+        </section>
 
-    </section>
+    </header>
+
+    <!-- *******JUMBOO****** -->
+    <div class="my_jumbotron">
+        <div class="container-fluid p-0">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../../assets/public/img/jumboo.jpg" class="d-block " alt="...">
+                        <div class="carousel-caption d-none d-md-block my_bg ">
+                            <h4>Enjoy every moment</h4>
+                            <p>Home is not where we live, but wherever we are understood.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/public/img/jumboo2.jpg" class="d-block " alt="...">
+                        <div class="carousel-caption d-none d-md-block my_bg ">
+                            <h4>Find you're story</h4>
+                            <p>Each House is a story that is not identical to any other.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/public/img/jumboo3.jpg3" class="d-block " alt="...">
+                        <div class="carousel-caption d-none d-md-block my_bg ">
+                            <h4>Feel at Home</h4>
+                            <p>Home is that place where, when you go, they always welcome you.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/public/img/jumboo4.jpg3" class="d-block " alt="...">
+                        <div class="carousel-caption d-none d-md-block my_bg ">
+                            <h4>Feel at Home</h4>
+                            <p>Home is that place where, when you go, they always welcome you.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
 
 </template>
 
@@ -63,5 +117,23 @@ section {
 
 .right-side>.carrello:hover {
     background-color: orange;
+}
+
+/* <!-- *******JUMBOO****** --> */
+.my_bg {
+    background-color: rgba(128, 128, 128, 0.534);
+    width: 40%;
+    margin: 0 auto
+}
+
+.my_jumbotron {
+    height: calc(100vh - 4.5rem);
+    /* overflow: hidden */
+}
+
+.my_jumbotron img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 </style>
